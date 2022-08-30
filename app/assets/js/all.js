e3bc2743988d3967252aff635899d0151c264ee8
rio wei <wutony0713@gmail.com>
2022-08-30 15:29:23 +0800

    作品頁OK 加入utilities

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,26 @@
 $(function() {
   console.log('Hello Bootstrap5');
 });
+
+
+
+var swiper = new Swiper(".mySwiper", {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: -25,
+    depth: 100,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".swiper-pagination"
+  }
+});
+
+// $('.artwork-control').imagesLoaded().progress( function() {
+//   $('.artwork-control').masonry(); // 渲染整體畫面
+// });
