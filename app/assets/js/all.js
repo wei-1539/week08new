45d9d39fcccc2e5748e073518d5d66de307e0033
RIO WEI <wutony0713@gmail.com>
2022-09-04 22:40:21 +0800

    swiper修改完成、首頁細微修正

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -3,22 +3,38 @@ $(function() {
 });
 
 
-
-var swiper = new Swiper(".mySwiper", {
-  effect: "coverflow",
-  grabCursor: true,
+const swiper = new Swiper('.swiper-container', {
   centeredSlides: true,
-  slidesPerView: "auto",
-  coverflowEffect: {
-    rotate: 50,
-    stretch: -25,
-    depth: 100,
-    modifier: 1,
-    slideShadows: false,
-  },
+  loop: true,
+  speed: 500,
+  // slidesPerView: 3,
+  spaceBetween: 10,
+  // autoplay: {
+  //     delay: 3000,
+  // },
   pagination: {
-    el: ".swiper-pagination"
-  }
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+    // navigation: {
+    //     nextEl: '.swiper-button-next',
+    //     prevEl: '.swiper-button-prev',
+    // },
+  breakpoints: {
+
+      640: {
+          slidesPerView: 1,
+      },
+      768: {
+          slidesPerView: 1,
+      },
+      1080: {
+          slidesPerView: 3,
+      },
+      1280: {
+          slidesPerView: 3,
+      },
+  },
 });
 
 // $('.artwork-control').imagesLoaded().progress( function() {
